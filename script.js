ccd15af02ea2d69ece833526edd7a23b286d00ee
jaspreet571 <138706287+jaspreet571@users.noreply.github.com>
2023-07-12 12:33:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,75 @@ function rollDice() {
     diceElements[i].textContent = rolls[i];
   }
 
-  // Calculate and update the score
-  const score = calculateScore(rolls);
-  scoreElement.textContent = score;
-}
+
 
 // Function to calculate the score
 function calculateScore(rolls) {
-  // TODO: Implement the scoring logic
-  // This function should return the total score based on the dice rolls.
-  // Refer to the rules of the Ferengi Game of Daboo to determine the scoring logic.
-  // You can use conditional statements, loops, and other JavaScript features to calculate the score.
-  // Feel free to define additional helper functions if needed.
+    let score = 0;
+
+    // Calculate the score based on the dice rolls
+    // You need to implement the scoring rules of the Ferengi Game of Daboo here.
+  
+    // Example scoring logic (modify according to the game rules):
+    for (const roll of rolls) {
+      if (roll === 1) {
+        score += 10;
+      } else if (roll === 6) {
+        score += 6;
+      } else {
+        score += roll;
+      }
+    }
+  
+    return score;
 }
+// Calculate and update the score
+const score = calculateScore(rolls);
+scoreElement.textContent = score;
+
+if (score >= 100) {
+  alert('Congratulations! You won!');
+  resetGame();
+} else if (rolls.includes(1)) {
+  alert('Oops! You rolled a 1. Game over!');
+  resetGame();
+}
+}
+
+
+// Function to reset the game
+function resetGame() {
+    // Reset dice rolls
+    for (const diceElement of diceElements) {
+      diceElement.textContent = '';
+    }
+  
+    // Reset the score
+    scoreElement.textContent = '0';
+  }
 
 // Event listener for the "Roll Dice" button
 const rollButton = document.getElementById('roll-button');
 rollButton.addEventListener('click', rollDice);
+
+// Event listener for the "Enter" key
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      rollDice();
+    }
+  });
+function playSoundEffect(soundFile) {
+  var audio = new Audio('sounds/' + soundFile); audio.play(); 
+} 
+
+function dabooAchieved() {
+ // Other code to handle the DABOO achievement 
+  playSoundEffect('daboo.mp3'); } 
+
+function updateNotification(message) {
+   var notificationElement = document.getElementById('notification');
+   notificationElement.innerText = message; } 
+
+function dabooAchieved() {
+   // Other code to handle the DABOO achievement 
+   updateNotification('Congratulations! You achieved DABOO!'); }
